03d31d58ab337cb339aa1fe9559b32263438580f
Vedant-29 <agarwalvedant42@gmail.com>
2024-07-05 19:05:53 +0530

    Easy qs -- 1

---
+++ c++Questions/Easy/SumInArray.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int arr[5];
+    int sum;
+
+    cout << "Enter an array of the size 5 = ";
+
+    for (int i=0; i < 5; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i=0; i < 5; i++) {
+        sum += arr[i];
+    }
+
+    cout << "The sum of the elements of the array is = " << sum << endl;
+
+    return 0;
+}
