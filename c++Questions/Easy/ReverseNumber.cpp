b287d258397defb526f91f44d7c386032eb2e475
Vedant-29 <agarwalvedant42@gmail.com>
2024-07-05 20:40:15 +0530

    Easy qs -- 3

---
+++ c++Questions/Easy/ReverseNumber.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    int reverse(int x) {
+        int ans = 0;
+        while(x != 0) {
+            if ((ans > INT_MAX/10) || ans < INT_MIN/10) return 0;
+            int digit = x % 10;
+            ans = digit + 10*ans;
+            x /= 10;
+        }
+
+        return ans;
+    }
+};
