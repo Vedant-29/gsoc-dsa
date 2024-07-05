0a88593a126f3d16b58eff433dadbdb0a1562d30
Vedant-29 <agarwalvedant42@gmail.com>
2024-07-05 22:23:45 +0530

    Easy qs -- 4 -- correction on file moved to medium

---
+++ c++Questions/Easy/CheckPalindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int original = x;
+        long long ans = 0;
+
+        while (x != 0) {
+            ans = ans*10 + x%10;
+            x /= 10;
+        }
+
+        return original == ans;
+    }
+};
